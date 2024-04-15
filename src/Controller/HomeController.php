353ced623a1e4519c3c2680fdcca997ff845f9a6
Lukabdx <luka.garcia@ynov.com>
2024-04-15 14:44:03 +0200

    Navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,8 +9,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'vitrine_home')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
