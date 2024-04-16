e9d36cb517a032f5122fb7d98b00f928bfe40689
Lukabdx <luka.garcia@ynov.com>
2024-04-16 10:56:34 +0200

    Docker seems to be working

--- .github/deploy.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/sh
-
-sudo -u www-data composer install --no-dev --optimize-autoloader
-sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
-sudo -u www-data php bin/console sass:build
-sudo -u www-data php bin/console asset-map:compile
-sudo -u www-data php bin/console cache:clear
-sudo -u www-data php bin/console cache:pool:clear --all
-sudo rm -rf var/cache/*
